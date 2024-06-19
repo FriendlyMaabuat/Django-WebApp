d47fcb60f2d5bf0d151f64765d752132a8c9d8c1
FriendlyMaabuat <friendlymaabuat25@gmail.com>
2024-06-19 14:40:46 +0800

    Setting up database models

--- job_application/models.py
+++ job_application/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Form(models.Model):
+    first_name = models.CharField(max_length=90)
+    last_name = models.CharField(max_length=90)
+    email = models.EmailField()
+    date = models.DateField()
+    occupation = models.CharField(max_length=90)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
